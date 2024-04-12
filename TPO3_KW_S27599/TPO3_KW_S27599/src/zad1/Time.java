9ae9e1b5bcc618eb4c7fb788bfac45d2a3b48948
s27599 <s27599@pjwstk.edu.pl>
2024-04-12 18:20:26 +0200

    YAML working time in progress

--- TPO3_KW_S27599/TPO3_KW_S27599/src/zad1/Time.java
+++ TPO3_KW_S27599/TPO3_KW_S27599/src/zad1/Time.java
@@ -7,5 +7,12 @@
 package zad1;
 
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Time {
+    public static String passed(String from, String to) {
+        LocalDate fromDate = LocalDate.parse(from,DateTimeFormatter.ofPattern("yyyy-MM-dd'T'gg:mm"));
+return String.valueOf(fromDate);
+    }
 }
